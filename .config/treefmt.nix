3119c7a648ef31fa0c0b627dc2e4653034afbdfb
fidgetingbits <fidgetingbits@noreply.codeberg.org>
2026-01-26 23:14:42 +0800

    feat(treefmt): add kdlfmt

--- .config/treefmt.nix
+++ .config/treefmt.nix
@@ -1,14 +1,26 @@
-{ ... }:
-{
+{ lib, ... }:
+rec {
   projectRootFile = "flake.nix";
   programs = {
+    kdlfmt = {
+      enable = true; # kdl
+    };
     nixfmt.enable = true; # nix
     ruff.enable = true; # python
     shfmt = {
       enable = true;
       indent_size = 4; # Seems not to pick up .editorconfig?
     };
-    # rustfmt.enable = true; # rust
     yamlfmt.enable = true; # yaml
+  };
+  settings.formatter = lib.optionalAttrs programs.kdlfmt.enable {
+    kdlfmt = {
+      options = [
+        "format"
+        # This is needed for niri config files not to break various quoted things
+        "--kdl-version"
+        "v1"
+      ];
+    };
   };
 }
