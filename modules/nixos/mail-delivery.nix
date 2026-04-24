534d6ceec2f0f364963af91ff254f30fafbced3c
emergentmind <emergentmind@noreply.codeberg.org>
2026-04-24 09:44:05 -0600

    feat: mail-delivery via msmtp or postfix-relay

---
+++ modules/nixos/mail-delivery.nix
@@ -0,0 +1,87 @@
+# Options to determine how msmtp or a postfix-relay will be used to deliver email notifications
+{ config, lib, ... }:
+let
+  cfg = config.introdus.mail-delivery;
+  secret = "passwords/${if cfg.useRelay then "postfix-relay" else "msmtp"}";
+in
+{
+  options.introdus.mail-delivery = {
+    enable = lib.mkEnableOption "Enable mail-delivery";
+    useRelay = lib.mkOption {
+      default = false;
+      type = lib.types.bool;
+      description = "Whether or not to use postfix mail relay for mail delivery";
+    };
+    emailFrom = lib.mkOption {
+      type = lib.types.str;
+      description = "Email address from which to send notifications";
+    };
+    user = lib.mkOption {
+      default = config.hostSpec.primaryUsername;
+      type = lib.types.str;
+      description = "Main user added to the group with mail delivery password access";
+    };
+    extraUsers = lib.mkOption {
+      default = [ ];
+      type = lib.types.listOf lib.types.str;
+      description = "Extra users added to the group with mail delivery password access";
+    };
+    group = lib.mkOption {
+      default = "msmtp-secret-users";
+      type = lib.types.str;
+      description = "Name of group controlling access to mail delivery password";
+    };
+    smtpUser = lib.mkOption {
+      type = lib.types.str;
+      description = "Username of smtp user";
+    };
+    smtpPort = lib.mkOption {
+      default = (if cfg.useRelay then 25 else 587);
+      type = lib.types.int;
+      description = "Port number for smtp server";
+    };
+    smtpHost = lib.mkOption {
+      type = lib.types.str;
+      description = "Domain of smtp server";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    users.groups.${cfg.group} = {
+      members = [ cfg.user ] ++ cfg.extraUsers;
+    };
+    sops.secrets."${secret}" = {
+      group = cfg.group;
+      mode = "0440";
+    };
+    programs.msmtp = {
+      enable = true;
+      setSendmail = true; # set the system sendmail to msmtp's
+      defaults = {
+        syslog = true;
+      };
+
+      accounts =
+        let
+          commonAttrs = {
+            auth = true;
+            tls = true;
+            tls_starttls = true;
+            port = cfg.smtpPort;
+            user = cfg.smtpUser;
+            from = cfg.emailFrom;
+            # Watch logs with `journalctl --facility=mail`
+            syslog = "LOG_MAIL";
+            #logfile /tmp/msmtp.log
+          };
+        in
+        {
+          "default" = {
+            host = cfg.smtpHost;
+            passwordeval = "cat ${config.sops.secrets.${secret}.path}";
+          }
+          // commonAttrs;
+        };
+    };
+  };
+}
